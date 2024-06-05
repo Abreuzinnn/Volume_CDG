25447d0fc2f0735686d8c6b4351cad04c2a95f9d
Abreuzinnn <ramonadoniss@gmail.com>
2024-06-05 11:53:16 -0300

    SH Update!!

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Atualiza os pacotes e instala as dependências necessárias
-apt-get update
-apt-get install -y libportaudio2 libasound2-dev libportaudio-dev libportaudiocpp0 portaudio19-dev
-
 # Instala os pacotes Python
 pip install -r requirements.txt
 
